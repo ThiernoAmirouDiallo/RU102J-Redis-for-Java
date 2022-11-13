4ba0ed8442eeaacd61a203273e6b459e77b281d0
Thierno Diallo <thiernoamiroudiallo@lyft.com>
2022-11-12 21:24:42 -0500

    Programming Challenge #1 solution improvement

--- src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteDaoRedisImpl.java
@@ -1,53 +1,60 @@
 package com.redislabs.university.RU102J.dao;
 
 import com.redislabs.university.RU102J.api.Site;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
-import java.util.*;
-
 public class SiteDaoRedisImpl implements SiteDao {
-    private final JedisPool jedisPool;
 
-    public SiteDaoRedisImpl(JedisPool jedisPool) {
-        this.jedisPool = jedisPool;
-    }
+	private final JedisPool jedisPool;
 
-    // When we insert a site, we set all of its values into a single hash.
-    // We then store the site's id in a set for easy access.
-    @Override
-    public void insert(Site site) {
-        try (Jedis jedis = jedisPool.getResource()) {
-            String hashKey = RedisSchema.getSiteHashKey(site.getId());
-            String siteIdKey = RedisSchema.getSiteIDsKey();
-            jedis.hmset(hashKey, site.toMap());
-            jedis.sadd(siteIdKey, hashKey);
-        }
-    }
+	public SiteDaoRedisImpl( JedisPool jedisPool ) {
+		this.jedisPool = jedisPool;
+	}
 
-    @Override
-    public Site findById(long id) {
-        try(Jedis jedis = jedisPool.getResource()) {
-            String key = RedisSchema.getSiteHashKey(id);
-            Map<String, String> fields = jedis.hgetAll(key);
-            if (fields == null || fields.isEmpty()) {
-                return null;
-            } else {
-                return new Site(fields);
-            }
-        }
-    }
+	// When we insert a site, we set all of its values into a single hash.
+	// We then store the site's id in a set for easy access.
+	@Override
+	public void insert( Site site ) {
+		try ( Jedis jedis = jedisPool.getResource() ) {
+			String hashKey = RedisSchema.getSiteHashKey( site.getId() );
+			String siteIdKey = RedisSchema.getSiteIDsKey();
+			jedis.hmset( hashKey, site.toMap() );
+			jedis.sadd( siteIdKey, hashKey );
+		}
+	}
 
-    // Challenge #1
-    @Override
-    public Set<Site> findAll() {
-        Set<Site> sites = new HashSet<>();
-        try ( Jedis jedis = jedisPool.getResource() ) {
-            for ( String siteHashKey : jedis.smembers( RedisSchema.getSiteIDsKey() ) ) {
-                sites.add( new Site( jedis.hgetAll( siteHashKey ) ) );
-            }
-        }
+	@Override
+	public Site findById( long id ) {
+		try ( Jedis jedis = jedisPool.getResource() ) {
+			String key = RedisSchema.getSiteHashKey( id );
+			Map<String, String> fields = jedis.hgetAll( key );
+			if ( fields == null || fields.isEmpty() ) {
+				return null;
+			} else {
+				return new Site( fields );
+			}
+		}
+	}
 
-        return sites;
-    }
+	// Challenge #1
+	@Override
+	public Set<Site> findAll() {
+		Set<Site> sites = new HashSet<>();
+		try ( Jedis jedis = jedisPool.getResource() ) {
+			for ( String siteHashKey : jedis.smembers( RedisSchema.getSiteIDsKey() ) ) {
+				Map<String, String> fields = jedis.hgetAll( siteHashKey );
+				if ( fields != null && !fields.isEmpty() ) {
+					sites.add( new Site( fields ) );
+				}
+			}
+		}
+
+		return sites;
+	}
 }
